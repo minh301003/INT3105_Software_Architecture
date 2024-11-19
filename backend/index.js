e86238914898bcd78d165b7a95f8635792765293
UlAntity <91358957+UlAntity@users.noreply.github.com>
2024-11-19 23:04:48 +0700

    update logic and grafana dashboard

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,11 @@ const PORT = 3002;
 
 const apiType = process.env.API_TYPE;
 
+// *** Prometheus metrics ***
 const register = new client.Registry();
-client.collectDefaultMetrics({ register }); 
+client.collectDefaultMetrics({ register }); // Collect default metrics
 
+// Custom metrics
 const serviceStatusGauge = new client.Gauge({
   name: 'service_status',
   help: 'Status of the service: 1 for UP, 0 for DOWN',
@@ -20,20 +22,31 @@ const serviceStatusGauge = new client.Gauge({
 });
 register.registerMetric(serviceStatusGauge);
 
-const httpRequestCounter = new client.Counter({
-  name: 'http_requests_total',
-  help: 'Total number of HTTP requests',
-  labelNames: ['method', 'route', 'status_code']
+const responseTimeHistogram = new client.Histogram({
+  name: 'http_response_time_miliseconds',
+  help: 'Response time in miliseconds',
+  labelNames: ['method', 'route', 'status_code'], // Để lọc theo route hoặc HTTP status
 });
-register.registerMetric(httpRequestCounter);
+register.registerMetric(responseTimeHistogram);
 
-// Middleware để đếm số lượng yêu cầu
+// Middleware
 app.use((req, res, next) => {
+  const start = Date.now(); // Start time
+
   res.on('finish', () => {
-    httpRequestCounter.inc({ method: req.method, route: req.path, status_code: res.statusCode });
+    const route = req.route && req.route.path ? req.route.path : req.originalUrl;
+    const duration = Date.now() - start; // Tính thời gian xử lý request
+    // Thời gian phản hồi
+    responseTimeHistogram.labels({ method: req.method, route: route, status_code: res.statusCode }).observe(duration);
   });
+
   next();
 });
+
+
+// *** End Prometheus metrics ***
+
+// *** Endpoints ***
 
 // Endpoint API for gold prices
 if (apiType === 'gold') {
@@ -43,11 +56,9 @@ if (apiType === 'gold') {
       const goldData = response.data;
       res.json(goldData);
       console.log("Successfully get gold data");
-      serviceStatusGauge.set({ service: 'gold-api' }, 1);
     } catch (error) {
       console.error(error);
       res.status(500).send('Error in gold API');
-      serviceStatusGauge.set({ service: 'gold-api' }, 0);
     }
   });
 }
@@ -65,24 +76,95 @@ if (apiType === 'currency') {
         }
         res.json(result);
         console.log("Successfully get currency data");
-        serviceStatusGauge.set({ service: 'currency-api' }, 1);
       });
     } catch (error) {
       console.log(error);
       res.status(500).send('Error in currency API');
-      serviceStatusGauge.set({ service: 'currency-api' }, 0);
     }
   });
 }
 
-// Metrics endpoint for Prometheus
+
+// Health check endpoints
+if (apiType === 'gold') {
+  app.get('/health/gold', async (req, res) => {
+    try {
+      await axios.get('http://api.btmc.vn/api/BTMCAPI/getpricebtmc?key=3kd8ub1llcg9t45hnoh8hmn7t5kc2v'); // !xóa btmc simulate api down
+      res.status(200).send('Gold API is UP');
+    } catch (error) {
+      res.status(500).send('Gold API is DOWN');
+    }
+  });
+}
+if (apiType === 'currency') {
+  app.get('/health/currency', async (req, res) => {
+    try {
+      const response = await axios.get('https://portal.vietcombank.com.vn/Usercontrols/TVPortal.TyGia/pXML.aspx?b=10');
+      console.log('Currency API response status:', response.status);
+      res.status(200).send('Currency API is UP');
+    } catch (error) {
+      console.log('Currency API error:', error.message);
+      res.status(500).send('Currency API is DOWN');
+    }
+  });
+  
+}
+
+let goldApiStatus = 0; // Biến lưu trạng thái của Gold API
+let currencyApiStatus = 0; // Biến lưu trạng thái của Currency API
+
+let currencyApiTimeoutCount = 0; // Biến đếm số lần timeout của Currency API
+
+// Thực hiện health check định kỳ mỗi 30s
+setInterval(async () => {
+  if (apiType === 'gold') {
+    try {
+      // Kiểm tra trạng thái của Gold API
+      const goldApiResponse = await axios.get('http://localhost:3002/health/gold', { timeout: 15000 });
+      goldApiStatus = goldApiResponse.status === 200 ? 1 : 0; // Cập nhật trạng thái Gold API
+    } catch (error) {
+      goldApiStatus = 0; // Nếu có lỗi, đánh dấu Gold API down
+    }
+  }
+  if (apiType === 'currency') {
+    // Kiểm tra trạng thái của Currency API
+    try {
+      const currencyApiResponse = await axios.get('http://localhost:3002/health/currency', { timeout: 15000 });
+      currencyApiStatus = currencyApiResponse.status === 200 ? 1 : 0; // Cập nhật trạng thái Currency API
+      currencyApiTimeoutCount = 0; // Reset biến đếm timeout
+    } catch (error) {
+      currencyApiTimeoutCount++; 
+      if (currencyApiTimeoutCount >= 2) {
+        currencyApiStatus = 0; // !Quá 2 lần timeout liên tiếp thì coi như Down
+      }
+    }
+  }
+}, 20000); // Health check mỗi 20s
+
+// Metrics endpoint cho Prometheus
 app.get('/metrics', async (req, res) => {
+  // Cập nhật trạng thái service vào serviceStatusGauge
+  if (apiType === 'gold') {
+    serviceStatusGauge.set({ service: 'gold-api' }, goldApiStatus);
+  }
+  if (apiType === 'currency') {
+    serviceStatusGauge.set({ service: 'currency-api' }, currencyApiStatus);
+  }
+
+  // Trả về tất cả metrics
   res.set('Content-Type', register.contentType);
   res.end(await register.metrics());
 });
 
+
+// *** End Endpoints ***
+
+// *** Health check ***
+// ? to be done
+// *** End Health check ***
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.use(cors({
